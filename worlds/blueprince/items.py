e730c078689d115e81f83942871d0d43285421f5
BatmenzDW <turtleterracotta@gmail.com>
2026-04-24 21:05:05 -0400

    implemented starting room generation

--- worlds/blueprince/items.py
+++ worlds/blueprince/items.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from random import random
 from typing import TYPE_CHECKING
+
+from .room_min_pieces import ENTRANCE_HALL_DRAFTABLE
 
 from .data_rooms import rooms, core_rooms, classrooms
 from .data_items import *
@@ -428,16 +431,23 @@ def create_all_items(world: BluePrinceWorld) -> None:
     else:
         to_precollect += special_shop_item_list
 
+    data_rooms.progressive_classroom = [world.create_item("Progressive Classroom") for _ in range(9)]
+
     room_item_list = [world.create_item(room) for room in rooms if room not in core_rooms and room not in ["Secret Garden", "Room 8"] and room not in classrooms]
+    starting_rooms = []
     if world.options.room_draft_sanity:
-        itempool += room_item_list
+        starting_rooms = world.random.choices(
+            [room for room in room_item_list if ROOM_PICK_POSITIONS_KEY in rooms[room.name] and set(rooms[room.name][ROOM_PICK_POSITIONS_KEY]) & ENTRANCE_HALL_DRAFTABLE] + data_rooms.progressive_classroom,
+            k=world.options.starting_room_amount.value,
+        )
+        itempool += [room for room in room_item_list if room not in starting_rooms]
+        to_precollect += starting_rooms
     else:
         # Precollects all room items, except for those that should be at their in-game locations, which are handled in locations.py
         to_precollect += [room for room in room_item_list if NONSANITY_LOCATION_KEY not in rooms[room.name] or rooms[room.name][NONSANITY_LOCATION_KEY] == STARTING_INVENTORY]
 
-    data_rooms.progressive_classroom = [world.create_item("Progressive Classroom") for _ in range(9)]
     if world.options.room_draft_sanity:
-        itempool += data_rooms.progressive_classroom
+        itempool += [room for room in data_rooms.progressive_classroom if room not in starting_rooms]
     else:
         to_precollect += data_rooms.progressive_classroom
 
--- worlds/blueprince/test/sanity/test_draft_sanity.py
+++ worlds/blueprince/test/sanity/test_draft_sanity.py
@@ -14,14 +14,20 @@ class TestDraftSanity(BluePrinceTestBase):
     options = {
         "room_draft_sanity": True,
         "goal_type": GoalType.option_room46,
+        "starting_room_amount": 3,
     }
 
-    def test_no_starting_room_items(self) -> None:
+    def test_starting_room_count(self) -> None:
+        n = 0
         for room in rooms:
             if room in core_rooms:
                 continue
+            if n < self.options["starting_room_amount"].value:
+                n += 1
+                continue
+
             self.assertFalse(self.multiworld.state.has(room, self.player))
-        self.assertFalse(self.multiworld.state.has("Progressive Classroom", self.player))
+        # self.assertFalse(self.multiworld.state.has("Progressive Classroom", self.player))
 
     def test_progressive_classroom(self) -> None:
         for cnum in range(1, 10):
@@ -32,41 +38,42 @@ class TestDraftSanity(BluePrinceTestBase):
             self.debug_print_regions_items_locations(True)
             self.assertTrue(self.can_reach_region(current), f"Should be able to reach {current} after collecting {cnum} Progressive Classrooms")
 
-    def test_fill(self):
-        """Generates a multiworld and validates placements with the defined options"""
-        if not (self.run_default_tests and self.constructed):
-            return
-        from Fill import distribute_items_restrictive
+    #  Copied here for debugging. This is run by the base class, but its easier to debug with it here
+    # def test_fill(self):
+    #     """Generates a multiworld and validates placements with the defined options"""
+    #     if not (self.run_default_tests and self.constructed):
+    #         return
+    #     from Fill import distribute_items_restrictive
 
-        # basically a shortened reimplementation of this method from core, in order to force the check is done
-        def fulfills_accessibility() -> bool:
-            locations = list(self.multiworld.get_locations(1))
-            state = CollectionState(self.multiworld)
-            while locations:
-                sphere: typing.List[Location] = []
-                for n in range(len(locations) - 1, -1, -1):
-                    if locations[n].can_reach(state):
-                        sphere.append(locations.pop(n))
+    #     # basically a shortened reimplementation of this method from core, in order to force the check is done
+    #     def fulfills_accessibility() -> bool:
+    #         locations = list(self.multiworld.get_locations(1))
+    #         state = CollectionState(self.multiworld)
+    #         while locations:
+    #             sphere: typing.List[Location] = []
+    #             for n in range(len(locations) - 1, -1, -1):
+    #                 if locations[n].can_reach(state):
+    #                     sphere.append(locations.pop(n))
                 
-                print(f"{[str(loc) for loc in sphere]}")
-                if not sphere:
-                    self.assertTrue(state.can_reach_region("Secret Garden", 1))
-                    self.assertTrue(state.can_reach_region("Room 8", 1))
-                    for room in data_other_locations.directory_rooms:
-                        self.assertTrue(state.has(room, self.player), f"Expected to have {room} in inventory for player {self.player}")
-                self.assertTrue(sphere or self.multiworld.worlds[1].options.accessibility == "minimal",
-                                f"Unreachable locations: {locations}")
-                if not sphere:
-                    break
-                for location in sphere:
-                    if location.item:
-                        state.collect(location.item, True, location)
-            return self.multiworld.has_beaten_game(state, self.player)
+    #             print(f"{[str(loc) for loc in sphere]}")
+    #             if not sphere:
+    #                 self.assertTrue(state.can_reach_region("Secret Garden", 1))
+    #                 self.assertTrue(state.can_reach_region("Room 8", 1))
+    #                 for room in data_other_locations.directory_rooms:
+    #                     self.assertTrue(state.has(room, self.player), f"Expected to have {room} in inventory for player {self.player}")
+    #             self.assertTrue(sphere or self.multiworld.worlds[1].options.accessibility == "minimal",
+    #                             f"Unreachable locations: {locations}")
+    #             if not sphere:
+    #                 break
+    #             for location in sphere:
+    #                 if location.item:
+    #                     state.collect(location.item, True, location)
+    #         return self.multiworld.has_beaten_game(state, self.player)
 
-        with self.subTest("Game", game=self.game, seed=self.multiworld.seed):
-            distribute_items_restrictive(self.multiworld)
-            call_all(self.multiworld, "post_fill")
-            self.assertTrue(fulfills_accessibility(), "Collected all locations, but can't beat the game.")
-            placed_items = [loc.item for loc in self.multiworld.get_locations() if loc.item and loc.item.code]
-            self.assertLessEqual(len(self.multiworld.itempool), len(placed_items),
-                                 "Unplaced Items remaining in itempool")
+    #     with self.subTest("Game", game=self.game, seed=self.multiworld.seed):
+    #         distribute_items_restrictive(self.multiworld)
+    #         call_all(self.multiworld, "post_fill")
+    #         self.assertTrue(fulfills_accessibility(), "Collected all locations, but can't beat the game.")
+    #         placed_items = [loc.item for loc in self.multiworld.get_locations() if loc.item and loc.item.code]
+    #         self.assertLessEqual(len(self.multiworld.itempool), len(placed_items),
+    #                              "Unplaced Items remaining in itempool")
